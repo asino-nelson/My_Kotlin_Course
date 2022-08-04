41af95d35937f01249d4aa394fd4ffc52538d9b7
asino-nelson <asinonelson276@gmail.com>
2022-08-04 18:13:01 +0300

    second commit

---
+++ src/main/kotlin/classes.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val customer1 = Customer("Nelson", 4_000)
+
+    customer1.Balance()
+
+}
+
+
+open class Customer(val name: String, var amountPaid :Int){
+
+    fun Balance(){
+
+        var itemPrice = 2_000
+        var balance = amountPaid - itemPrice
+
+        println("Dear $name your balance is $balance")
+
+    }
+
+}
